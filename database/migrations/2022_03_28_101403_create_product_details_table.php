7dc72ac5ac079b5b7aff771524c928865476f7d3
Rimon0000 <rimonron.ad@gmail.com>
2022-03-28 16:17:29 +0600

    Product Detail Section Updated

---
+++ database/migrations/2022_03_28_101403_create_product_details_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_details', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('product_id');
+            $table->text('short_details');
+            $table->text('long_details');
+            $table->text('faq')->nullable();
+            $table->text('warranty_policy')->nullable();
+            $table->integer('status')->default(1);
+            $table->integer('created_by');
+            $table->integer('updated_by')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_details');
+    }
+}
