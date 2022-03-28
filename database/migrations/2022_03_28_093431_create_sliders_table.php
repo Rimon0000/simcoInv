badafdbe06ce7af50e35b669ea9096bed2572a1d
Rimon0000 <rimonron.ad@gmail.com>
2022-03-28 15:38:38 +0600

    Slider section updated

---
+++ database/migrations/2022_03_28_093431_create_sliders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('slider_name')->unique();
+            $table->string('slider_img');
+            $table->string('slider_alt')->nullable();
+            $table->integer('product_code')->default(0);
+            $table->integer('status')->default(1);
+            $table->integer('created_by');
+            $table->integer('updated_by')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sliders');
+    }
+}
