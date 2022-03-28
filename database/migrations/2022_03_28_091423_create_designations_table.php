df3d805706f6bc99111421b6d8c27d112978042e
Rimon0000 <rimonron.ad@gmail.com>
2022-03-28 15:16:31 +0600

    Designation Updated

---
+++ database/migrations/2022_03_28_091423_create_designations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDesignationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('designations', function (Blueprint $table) {
+            $table->id();
+
+            
+            $table->string('designation')->unique();
+            $table->string('short_name')->nullable()->unique();
+            $table->integer('status')->default(1);
+            $table->integer('created_by');
+            $table->integer('updated_by')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('designations');
+    }
+}
