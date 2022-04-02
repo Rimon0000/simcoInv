1d5ff9b967838f8fed0bc393ff02ca6a8bc05ee2
Faisal A. Salam <faisaltez@gmail.com>
2022-04-03 05:05:04 +0600

    update route

--- routes/web.php
+++ routes/web.php
@@ -44,10 +44,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/admin', function () {
     return view('welcome');
 });
 
