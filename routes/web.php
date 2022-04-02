800615461431b3faff9bb0200c6721995805fa3c
Personal <faisaltez@gmail.com>
2022-04-03 04:49:56 +0600

    service list updated

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/admin', function () {
     return view('welcome');
 });
 
